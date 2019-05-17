06aa92b072bf6822b19a0795c51ea4b74642ca59
Rony <elfantasmax2146@gmail.com>
2019-05-16 20:37:42 -0500

    agregando bebidas

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+ window.addEventListener('load', () => {
+
+	console.log('hey')
+
+	let inicio = document.getElementById('logo')
+	inicio.addEventListener('click' , () => {
+  
+        window.location = "index.html"
+
+	})
+})
